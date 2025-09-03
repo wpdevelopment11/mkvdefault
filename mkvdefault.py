3e320b5272e43e6cb92fad81169d2790f3af7cde
wpdevelopment11 <wpdevelopment11@gmail.com>
2025-09-03 21:58:23 +0300

    Improve code and add more features
    You can specify the track type on the command-line,
    'audio' for example, to make it easier to match the new default.
    
    Improved printing of the tracks, before and after modification.
    The tracks are not printed unless the '--verbose' flag is given.
    
    When a new default track is chosen, the 'default' flag in other
    tracks of the same type is removed.
    Now, if you pass the '--same-lang' on the command-line
    the 'default' flag will only be removed if the language is the same,
    tracks in other languages will not be modified at all.
    
    The error handling is improved, and the code should be robust now.

--- mkvdefault.py
+++ mkvdefault.py
@@ -1,77 +1,149 @@
+"""A wrapper over mkvpropedit to quickly change the default track in an mkv file.
+"""
+
+from functools import reduce
+
 import argparse
 import glob
 import json
 import os
 import os.path
 import subprocess
+import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("new_default", metavar="new-default")
-parser.add_argument("files", nargs="*", metavar="file.mkv")
-args = parser.parse_args()
+def exit_if_err(status, output, message, filename):
+    if status == 0:
+        return
+    error = "mkvmerge exited with a non-zero code: {}\nWhile trying to {} file '{}'.".format(status, message, filename)
+    print(error)
+    for error in output["errors"]:
+        print(error, end="")
+    for warning in output["warnings"]:
+        print(warning, end="")
+    sys.exit(1)
 
-files, new_default = args.files, args.new_default
+def print_and_exit(message):
+    print(message)
+    sys.exit(1)
 
-files = files if files else glob.iglob(os.path.join(os.getcwd(), "**", "*.mkv"), recursive=True)
+def print_tracks(tracks):
+    for track_num, track in tracks:
+        name_maxlen = reduce(lambda maxlen, name: max(maxlen, len(name)),
+                             [track["properties"]["track_name"] for _, track in tracks if track["properties"].get("track_name")],
+                             0)
+        default_str = "default" if track["properties"]["default_track"] else ""
+        track_name = track["properties"].get("track_name", "")
+        print("{:<2}) {:<9} ({}) {:<{}} {}".format(track_num,
+                                                   track["type"],
+                                                   track["properties"]["language"],
+                                                   track_name,
+                                                   name_maxlen,
+                                                   default_str))
 
-if new_default.isdecimal():
-    new_default = int(new_default)
-    def matchfunc(track_num, track):
-        return track_num == new_default
-else:
-    def matchfunc(track_num, track):
-        return (new_default.casefold() in track["properties"]["track_name"].casefold()
-                if track["properties"].get("track_name") else False)
+def tracks_of_type(tracks, track_type=None):
+    return [(i+1, track) for i, track in enumerate(tracks)
+            if track_type is None or track["type"] == track_type]
 
-for file in files:
-    proc = subprocess.run(["mkvmerge", "-J", file], capture_output=True)
+def mkvmerge(file):
+    proc = subprocess.run(["mkvmerge", "-J", file], capture_output=True, encoding="utf-8")
     mkv_info = json.loads(proc.stdout)
+    exit_if_err(proc.returncode, mkv_info, "print tracks after modification in", file)
+    return mkv_info
 
-    tracks = [(i+1, track) for i, track in enumerate(mkv_info["tracks"]) if matchfunc(i+1, track)]
-    if len(tracks) > 1:
-        print("Multiple tracks match")
-        exit(1)
-    elif len(tracks):
-        default_track_num, track = tracks[0]
-        default_type = track["type"]
-        tracks_to_modify = [(i+1, i+1 == default_track_num)
-                         for i, track in enumerate(mkv_info["tracks"])
-                         if track["type"] == default_type and ((i+1 != default_track_num and track["properties"]["default_track"])
-                                                            or (i+1 == default_track_num and not track["properties"]["default_track"]))]
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--same-lang", action="store_true")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-t", "--track-type", choices=["audio", "subtitles", "video"])
+    parser.add_argument("name_or_number", metavar="name-or-number")
+    parser.add_argument("files", nargs="*", metavar="file.mkv")
+    return parser
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    files, name_or_number = args.files, args.name_or_number
+
+    files = files if files else glob.iglob(os.path.join(os.getcwd(), "**", "*.mkv"), recursive=True)
+
+    if name_or_number.isdecimal():
+        def matchfunc(track):
+            return track[0] == name_or_number
+        name_or_number = int(name_or_number)
+        if name_or_number <= 0:
+            print_and_exit("Track numbers starts at 1.")
     else:
-        print("No tracks match")
-        exit(1)
+        def matchfunc(track):
+            track = track[1]
+            return (name_or_number.casefold() in track["properties"]["track_name"].casefold()
+                    if track["properties"].get("track_name") else False)
 
-    cmd = ["mkvpropedit", file]
+    for file in files:
+        mkv_info = mkvmerge(file)
 
-    if tracks_to_modify:
-        for track_num, default in tracks_to_modify:
-            cmd += ["--edit", "track:{}".format(track_num), "--set", "flag-default={:d}".format(default)]
+        if mkv_info["container"]["type"] != "Matroska":
+            print("The file '{}' is not an mkv file.".format(file))
+            sys.exit(1)
 
-        proc = subprocess.run(cmd, capture_output=True)
+        tracks = [track for track in tracks_of_type(mkv_info["tracks"], args.track_type)
+                if matchfunc(track)]
+        if len(tracks):
+            if len(tracks) > 1:
+                # here tracks always have a name
+                track_numbers = [i for i, _ in tracks]
+                print("Multiple tracks match. Select the track using its number:")
+                while True:
+                    print_tracks(tracks)
+                    num = input()
+                    try:
+                        num = int(num)
+                        i = track_numbers.index(num)
+                        default_track_num, track = tracks[i]
+                    except ValueError:
+                        print("Invalid track number. Try again!")
+                        continue
+                    break
+            else:
+                default_track_num, track = tracks[0]
+            default_type = track["type"]
+            default_lang = track["properties"]["language"]
+            tracks_to_modify = [(i, i == default_track_num)
+                            for i, track in tracks_of_type(mkv_info["tracks"], default_type)
+                            if ((i != default_track_num and track["properties"]["default_track"]
+                                    and (not args.same_lang or track["properties"]["language"] == default_lang))
+                                or (i == default_track_num and not track["properties"]["default_track"]))]
+        else:
+            print("No tracks match in file '{}'.".format(file))
+            continue
 
-        if proc.returncode:
-            print("mkvpropedit Non-zero exit code: {}".format(proc.returncode))
+        cmd = ["mkvpropedit", file]
 
-    # Print result
+        if tracks_to_modify:
+            for track_num, default in tracks_to_modify:
+                cmd += ["--edit", "track:{}".format(track_num), "--set", "flag-default={:d}".format(default)]
 
-    proc = subprocess.run(["mkvmerge", "-J", file], capture_output=True)
-    mkv_info = json.loads(proc.stdout)
+            proc = subprocess.run(cmd, capture_output=True, encoding="utf-8")
 
-    tracks = [track for track in mkv_info["tracks"] if track["type"] == default_type]
+            if proc.returncode:
+                print_and_exit("mkvpropedit exited with a non-zero code: {}\nWhile trying to modify the file '{}'.\n{}".format(proc.returncode, file, proc.stdout))
 
-    def sort_key(track):
-        props = track["properties"]
-        track_name = props["track_name"] if props.get("track_name") else ""
-        is_default = props["default_track"]
-        return (not is_default, track_name)
+        # Print result
 
-    tracks.sort(key=sort_key)
+        if not args.verbose:
+            continue
 
-    print(os.path.basename(file))
-    for track in tracks:
-        props = track["properties"]
-        track_name = props["track_name"] if props.get("track_name") else ""
-        is_default = props["default_track"]
-        print("{:50} (default: {:d})".format(track_name, is_default))
-    print()
+        prev = tracks_of_type(mkv_info["tracks"], default_type)
+        mkv_info = mkvmerge(file)
+        current = tracks_of_type(mkv_info["tracks"], default_type)
+
+        print_message = "tracks of type '{}' in file '{}':".format(default_type, file)
+
+        print("Previous {}".format(print_message))
+        print_tracks(prev)
+        print("Current {}".format(print_message))
+        print_tracks(current)
+        print()
+
+if __name__ == "__main__":
+    main()
